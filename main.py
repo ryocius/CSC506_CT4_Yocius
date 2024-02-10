b8475e5b0b0e825cfaff89b9339e54277075de50
ryocius <158761467+ryocius@users.noreply.github.com>
2024-02-10 11:09:44 -0500

    Linked List Queue implemented and tested

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Object:
+    def __init__(self, name):
+        self.name = name
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def printName(self):
+        print(self.name)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Create a Linked List Node class designed for use in a Queue
+class LinkedListNode:
+    def __init__(self, object):
+        self.object = object
+        self.next = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class LinkedListQueue:
+    def __init__(self):
+        self.head = None
+
+    # Enqueue, or insert at beginning of linked list
+    def enqueue(self, object):
+        new_node = LinkedListNode(object)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def listLength(self):
+        size = 0
+        if(self.head):
+            thisNode = self.head
+            while(thisNode):
+                size += 1
+                thisNode = thisNode.next
+        return size
+
+    # Dequeue, or remove last node from linked list
+    def dequeue(self):
+        if self.head is None:
+            return None
+
+        thisNode = self.head
+        if self.listLength() == 1:
+            return thisNode
+        else:
+            while(thisNode.next.next):
+                thisNode = thisNode.next
+
+        thisNode.next = None
+        return thisNode
+
+
+
+obj1 = Object("obj1")
+obj2 = Object("obj2")
+obj3 = Object("obj3")
+obj4 = Object("obj4")
+
+list = LinkedListQueue()
+list.enqueue(obj1)
+list.dequeue().object.printName()
+
+list.enqueue(obj1)
+list.enqueue(obj2)
+list.enqueue(obj3)
+list.enqueue(obj4)
+list.dequeue().object.printName()
+list.dequeue().object.printName()
+list.dequeue().object.printName()
+list.dequeue().object.printName()
+list.dequeue().object.printName()
